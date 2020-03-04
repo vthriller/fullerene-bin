aa06ae57b05e5ca5f2ff8750d75cd2aa46b0447b
vthriller <unixway.drive+git@gmail.com>
2020-03-04 22:41:04 +0300

    customizable width and height

--- fullerene.go
+++ fullerene.go
@@ -4,9 +4,29 @@ import (
 	"bytes"
 	"github.com/wcharczuk/go-chart"
 	"net/http"
+	"strconv"
 )
 
+func int_param(req *http.Request, key string, implicit int) (int, error) {
+	value := req.URL.Query().Get(key)
+	if len(value) == 0 {
+		return implicit, nil
+	}
+	return strconv.Atoi(value)
+}
+
 func test(w http.ResponseWriter, req *http.Request) {
+	width, err := int_param(req, "w", 800)
+	if err != nil {
+		http.Error(w, "width: " + err.Error(), 500)
+		return
+	}
+	height, err := int_param(req, "h", 480)
+	if err != nil {
+		http.Error(w, "height: " + err.Error(), 500)
+		return
+	}
+
 	graph := chart.Chart{
 		Series: []chart.Series{
 			chart.ContinuousSeries{
@@ -14,10 +34,12 @@ func test(w http.ResponseWriter, req *http.Request) {
 				YValues: []float64{3.14159, 2.71828, -1, 0},
 			},
 		},
+		Width: width,
+		Height: height,
 	}
 
 	buffer := bytes.NewBuffer([]byte{})
-	err := graph.Render(chart.PNG, buffer)
+	err = graph.Render(chart.PNG, buffer)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
