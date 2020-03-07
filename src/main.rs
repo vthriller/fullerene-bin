80b173e3ba66ac4787d0dbe4bcd01b46eb005c2b
vthriller <unixway.drive+git@gmail.com>
2020-03-07 03:47:03 +0300

    fetch data from local prometheus and render single metric

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ use plotters::prelude::*;
 use itertools::Itertools;
 use std::ops::Range;
 
+use std::collections::HashMap;
+
+use reqwest::blocking::Client;
+use chrono::prelude::*;
+use chrono::Duration;
+use serde::Deserialize;
+
 fn iter_to_range<I: Iterator<Item = f64>>(i: I) -> Range<f64> {
 	use itertools::MinMaxResult::*;
 	match i.minmax() {
@@ -11,13 +18,60 @@ fn iter_to_range<I: Iterator<Item = f64>>(i: I) -> Range<f64> {
 	}
 }
 
+#[allow(non_camel_case_types, non_snake_case, dead_code)]
+#[derive(Deserialize)]
+#[serde(tag = "status")]
+enum PromQueryRangeStatus {
+	success { data: PromQueryRangeResult },
+	error {
+		errorType: String,
+		error: String,
+	},
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Deserialize)]
+#[serde(tag = "resultType")]
+enum PromQueryRangeResult {
+	matrix { result: Vec<PromMetric> },
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Deserialize, Debug)]
+#[serde(tag = "resultType")]
+struct PromMetric {
+	metric: HashMap<String, String>,
+	values: Vec<(f64, String)>,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let data = vec![
-		(1., 3.14159),
-		(2., 2.71828),
-		(3., -1.),
-		(4., 0.),
-	];
+	let now = Utc::now();
+
+	let client = Client::new();
+	let resp = client.get("http://127.0.0.1:9090/api/v1/query_range")
+		.query(&[
+			("query", "sum(rate(node_cpu{instance=\"localhost:9100\"} [5m])) by (mode)"),
+			("start", &format!("{}", (now - Duration::hours(1)).timestamp())),
+			("end", &format!("{}", now.timestamp())),
+			("step", "5"),
+		])
+		.send()?
+        .json::<PromQueryRangeStatus>()?;
+
+	let data = match resp {
+		PromQueryRangeStatus::success { data } => match data {
+			PromQueryRangeResult::matrix { result } => result,
+		},
+		PromQueryRangeStatus::error { error, .. } => {
+			println!("error: {}", error);
+			return Ok(());
+		},
+	};
+
+	for metric in data {
+	let data: Vec<(f64, f64)> = metric.values.into_iter()
+		.filter_map(|(k, v)| v.parse().ok().map(|v| (k, v)))
+		.collect();
 
 	let root = BitMapBackend::new("test.png", (800, 480)).into_drawing_area();
 	root.fill(&WHITE)?;
@@ -33,5 +87,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 	chart.configure_mesh().draw()?;
 	chart.draw_series(LineSeries::new(data, &RED))?;
 
+	return Ok(())
+	}
+
+	// XXX no time series
 	Ok(())
 }
