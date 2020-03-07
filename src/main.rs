555036a50658ac75bf5197a6fc17d4334a17a166
vthriller <unixway.drive+git@gmail.com>
2020-03-07 17:41:12 +0300

    serve image via http

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,34 @@
 mod prom;
 mod render;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+
+type Error = Box<dyn std::error::Error + Sync + Send>;
+
+async fn handle(_req: Request<Body>) -> Result<Response<Body>, Error> {
 	let data = prom::fetch()?;
-	let _ = render::render(data);
-	Ok(())
+	let img = render::render(data)?;
+
+	let resp = Response::builder()
+		.status(200)
+		.header("Content-Type", "image/png")
+		.body(Body::from(img))?;
+
+	Ok(resp)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+	let addr = SocketAddr::from(([127, 0, 0, 1], 12345));
+
+	let make_service = make_service_fn(|_conn| async {
+		Ok::<_, Error>(service_fn(handle))
+	});
+
+	let server = Server::bind(&addr)
+		.serve(make_service);
+
+	Ok(server.await?)
 }
