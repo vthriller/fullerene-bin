83c7d3e9338e12c33261fc37d79a1cb56079a245
vthriller <unixway.drive+git@gmail.com>
2020-03-07 04:52:24 +0300

    different colors for each metric

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use plotters::prelude::*;
+use hsluv::hsluv_to_rgb;
+
 use itertools::Itertools;
 use std::ops::Range;
 
@@ -42,6 +44,19 @@ enum PromQueryRangeResult {
 struct PromMetric {
 	metric: HashMap<String, String>,
 	values: Vec<(f64, String)>,
+}
+
+// Generates colors using perceptually uniform color space (HSLuv in this case)
+fn colors() -> impl Iterator<Item = RGBColor> {
+	(0..5).map(|i| {
+		let (r, g, b) = hsluv_to_rgb(((i*60) as f64, 80., 70.));
+		RGBColor(
+			(r * 255.) as u8,
+			(g * 255.) as u8,
+			(b * 255.) as u8,
+		)
+	})
+	.cycle()
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -91,8 +106,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 	chart.configure_mesh().draw()?;
 
-	for data in data {
-		chart.draw_series(LineSeries::new(data, &RED))?;
+	for (data, color) in data.into_iter().zip(colors()) {
+		chart.draw_series(LineSeries::new(data, &color))?;
 	}
 
 	Ok(())
