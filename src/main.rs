98407de768993fbc00f3d5c772f58e09b6e51b40
vthriller <unixway.drive+git@gmail.com>
2020-03-08 19:28:33 +0300

    query string: optional width, height

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ use hyper::service::{make_service_fn, service_fn};
 use chrono::prelude::*;
 use chrono::Duration;
 
+use serde::Deserialize;
+use serde_qs as qs;
+
 use image::{
 	ColorType,
 	png::PNGEncoder,
@@ -16,7 +19,21 @@ use image::{
 
 type Error = Box<dyn std::error::Error + Sync + Send>;
 
-async fn handle(_req: Request<Body>) -> Result<Response<Body>, Error> {
+#[derive(Debug, Deserialize)]
+struct QueryParams {
+	w: Option<u32>,
+	h: Option<u32>,
+}
+
+async fn handle(req: Request<Body>) -> Result<Response<Body>, Error> {
+	let params: QueryParams = match qs::from_str(req.uri().query().unwrap_or("")) {
+		Ok(params) => params,
+		// this error is not Sync, hence this
+		Err(_) => return Err("query string")?,
+	};
+	let w = params.w.unwrap_or(800);
+	let h = params.h.unwrap_or(480);
+
 	let now = Utc::now();
 	let data = prom::fetch(
 		now - Duration::hours(1),
@@ -25,13 +42,13 @@ async fn handle(_req: Request<Body>) -> Result<Response<Body>, Error> {
 	let img = render::render(
 		data,
 		now - Duration::hours(1) .. now,
-		800, 480,
+		w, h,
 	)?;
 
 	let mut png = vec![];
 	PNGEncoder::new(&mut png)
 		.encode(
-			&img, 800, 480,
+			&img, w, h,
 			ColorType::RGB(8),
 		)?;
 
